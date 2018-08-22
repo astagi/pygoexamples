ddd98edd9f1e4a85735258af28b70bb485ad5091
Δndrea Stagi <stagi.andrea@gmail.com>
2018-08-22 14:43:08 +0200

    Update pureperfthreads.py

--- performances/pureperfthreads.py
+++ performances/pureperfthreads.py
@@ -10,8 +10,8 @@ def countdown(n):
 if __name__ == '__main__':
     pool = Pool(processes=2)
     start = time.time()
-    r1 = pool.apply_async(countdown, [COUNT//2])
-    r2 = pool.apply_async(countdown, [COUNT//2])
+    r1 = pool.apply_async(countdown, [COUNT/2])
+    r2 = pool.apply_async(countdown, [COUNT/2])
     pool.close()
     pool.join()
     end = time.time()
