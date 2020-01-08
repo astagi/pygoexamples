0738e079ad5e4bb7350c03357c33182ec4d6e8ec
Andrea Stagi <stagi.andrea@gmail.com>
2020-01-08 19:25:40 +0100

    More docs

--- 3_performances/try_hello.py
+++ 3_performances/try_hello.py
@@ -11,6 +11,17 @@ for i in range (0, 900000):
 end = time.time()
 print('\n\nPure Hello: Time taken in seconds -', end - start)
 
+"""
+The following call throws an error if called with GODEBUG=cgocheck=1
+
+panic: runtime error: cgo result has Go pointer
+
+goroutine 17 [running, locked to thread]:
+main._cgoexpwrap_80580a90e820_sayHello.func1(0xc000048e50)
+	_cgo_gotypes.go:69 +0x51
+main._cgoexpwrap_80580a90e820_sayHello(0x10d4b5de0, 0x7, 0xc00001a0f0, 0xd)
+	_cgo_gotypes.go:71 +0xfd
+"""
 for i in range (0, 900000):
     print('\r' + say_hello("PyPizza"), end='')
 end = time.time()
--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 # Extend Python in Go Examples
 
+## What you need
+
+For this experiment you need:
+
+- Python 3.8 + Python source code
+- Golang 1.13.5
+
+Setup LIBRARY_PATH on your system. For me on Mac is
+
+```sh
+export LIBRARY_PATH=/Library/Frameworks/Python.framework/Versions/3.8/lib
+```
+
 ## Install dependencies
 
 If you want to run examples please install Python dependencies in your virtualenv
 
-    pip install -r requirements.txt
-
+```sh
+pip install -r requirements.txt
+```
 
 ## Chapter 1 - From C to Python
 
